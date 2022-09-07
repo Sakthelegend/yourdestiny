15706080a118d670f63642856d4cb105823ddb76
Sakthelegend <79069949+Sakthelegend@users.noreply.github.com>
2022-09-07 23:52:23 +0700

    added new files

---
+++ script.js
@@ -0,0 +1,87 @@
+
+
+
+function check(){
+    var dname=document.getElementById("_name").value;  
+    var person=dname.toLowerCase();
+    var nme=person.charAt(0);
+
+    if(nme=="a"){
+        document.getElementById("p2").innerHTML="People whose name starts with “A” have leadership qualities and have the power to attain heights of success through their wisdom and intelligence. These people are very ambitious, straight-forward and more focused towards their destiny. Moreover, these people like others to follow them and work according to their choices.";
+    }
+    else if(nme=="b"){
+        document.getElementById("p2").innerHTML="Names starting with the letter “B” are hyper-sensitive and like to be pampered by others. These people want attention all the time and if you ignore them, you might see their worst side. Loving and caring is all that the letter “B” stands for. This is the reason they enjoy being with family and friends all the time.";
+    }
+    else if(nme=="c"){
+        document.getElementById("p2").innerHTML="People comming under the letter “C” are good motivators and their communication skills can impress anybody. These people have a pool of talent which they want to utilise by helping others. They are highly optimistic, full of creative energy and enthusiasm. Moreover, their way of talking and communicating with people gives a hint that they can great motivational speakers.";
+    }
+    else if(nme=="d"){
+        document.getElementById("p2").innerHTML="As the letter suggests “D” means discipline and now you can guess what type of people they are. These people are very disciplined and business-minded. You need to think twice before opening your mouth in front of them. You can call it their ego but it’s their personality which they cannot change. These people believe in life rather than just dreaming to achieve something.";
+    }
+    else if(nme=="e"){
+        document.getElementById("p2").innerHTML="People whose name starts with “E” are quiet imaginative and intellectual. These people like to be more into themselves driving their own imagination rather than thinking of the outside world. You can often call them creative when it comes to generating an idea or brainstorming about new things. These people can be good artists, music directors, writers etc.";
+    }
+    else if(nme=="f"){
+        document.getElementById("p2").innerHTML="Names starting with the letter “F” are very short-tempered and can get angry even on a small bit of thing. These people hate lies and that is the reason they have anger issues. Apart from this, they are very caring, lovable, loyal and honest. But if you are in their bad terms, they will it sure that you see their worst side.";
+    }
+    else if(nme=="g"){
+        document.getElementById("p2").innerHTML="People falling under the letter “G” are perfectionists and they want everything to be done in a proper form. These people are highly active and cannot sit on a single chair all day thinking unnecessary things. Due to their perfectionist ways, G people are extremely selective while choosing a partner. They will prefer to stay single rather than choosing a partner who doesn’t match their mentality.";
+    }
+    else if(nme=="h"){
+        document.getElementById("p2").innerHTML="People starting with the letter “H” are nature lovers and think more about the surrounding and environment. These people are very hygienic and want everything neat & clean around them. You will often see them planting a tree around the roadside or watering the plants or participating in some clean environment activities.";
+    }
+    else if(nme=="i"){
+        document.getElementById("p2").innerHTML="People whose name starts with “I” are more inclined towards technology and modern things. These people are very fond of fashionable and branded things. You will often see them wearing branded clothes, shoes, wristwatch etc. Apart from this, these people devote much time to using Social Media, playing online games and looking for new things on the Internet. You can also call them social butterflies.";
+    }
+    else if(nme=="j"){
+        document.getElementById("p2").innerHTML="If your name starts with the letter “J” it means you have great physical energy and that becomes the biggest reason for your success. These people are very ambitious, focused and extremely active. Thus, you want a life partner who can help you excel in future. These people wish to have an intelligent person as a better half. These people can be good wrestlers, athletes, pilots etc.";
+    }
+    else if(nme=="k"){
+        document.getElementById("p2").innerHTML="People falling under the letter “K” are pretty romantic, secretive, caring and have the power to hide their emotions in front of others. These people are very serious when it comes to relationships and love related matters. If you are looking for a life partner find someone starting with letter K. Pure hearted people who can help you in every situation without any greed or selfishness.";
+    }
+    else if(nme=="l"){
+        document.getElementById("p2").innerHTML="People whose name starts with “L” are generally overthinkers who like to get into the roots of a certain situation. These people look deep into the matter till the time it gets solved. However, this nature can prove harmful for them sometimes as thinking more about the things can affect your health and mind as well.";
+    }
+    else if(nme=="m"){
+        document.getElementById("p2").innerHTML="This is quite interesting. People starting with the letter “M” are very lazy and slow. You can also call them human Tortoise. These people just want rest all day doing nothing and eating food only. You will often see them lying on bed with some food item in mouth and just criticising the things. However, these people are very lovable, emotional and sometimes a bit humorous too.";
+    }
+    else if(nme=="n"){
+        document.getElementById("p2").innerHTML="People falling under the letter “N” are more like independent birds who can travel anywhere without any boundaries or limitations. These people want full freedom of life doing anything they want. However, it doesn’t mean that they are useless, they are also very ambitious. It’s just their nature that makes them quite different from the world.";
+    }
+    else if(nme=="o"){
+        document.getElementById("p2").innerHTML="People with names starting with the letter “O” are extremely focused, and not afraid to work hard towards their goals. They have a very high level of morals and ensure that they always follow them with every decision in their lives. However, they are extremely sensitive and often have strong spiritual beliefs. For these individuals, nothing is impossible to accomplish.";
+    }
+    else if(nme=="p"){
+        document.getElementById("p2").innerHTML="People whose name starts with the letter “P” are very talkative, humorous and funny. These people have a very good sense of humour and very difficult to understand what they are saying as they might taunt you in a sarcastic way. You will often see them communicating with the people in a very funnier and informal way. However, these people are very intelligent and clever.";
+    }
+    else if(nme=="q"){
+        document.getElementById("p2").innerHTML="People falling under the letter “Q” are highly alcoholic and spend most of their time drinking wine with divine music. These people are more into themselves or you can say lost into their own world. They just want a bottle of liquor in their hands roaming here and there enjoying their own self. However, these people are quite imaginative and bold when it comes to communicating with others.";
+    }
+    else if(nme=="r"){
+        document.getElementById("p2").innerHTML="People whose name starts with the letter “R” are extremely workaholic who wants to spend their whole life completing tasks only. These people are hard workers and they will do anything to complete their task on time. However, their thinking is limited, they just want to work rather than looking at the wider picture and achieving something big. This is one of their drawbacks which they need to concentrate upon.";
+    }
+    else if(nme=="s"){
+        document.getElementById("p2").innerHTML="People whose name starts with “S” are multi-talented and can shine in any field whether it is acting, politics, business, sports or any creative field. These people have only one motive in life i.e to achieve success, fame, money. Thus, these people put in a lot of hard work to gain success professionally. However, when it comes to love, you will often see them standing alone or ditched by someone.";
+    }
+    else if(nme=="t"){
+        document.getElementById("p2").innerHTML="People whose name starts with the Letter “T” are very good at managing things and working efficiently in different situations. These people want everything to be done in a very organised manner and within a particular time period. These people are mentally strong and always try to learn new things and generate new ideas of manging things in different situations.";
+    }
+    else if(nme=="u"){
+        document.getElementById("p2").innerHTML="People starting with the letter “U” are quite notorious and always try to find shortcuts to achieve success. These people are very smart and tricky when it comes to dealing with difficult situations. However, this nature can prove harmful for them as they might get caught doing some unlawful activity or you can also see them behind the bars.";
+    }
+    else if(nme=="v"){
+        document.getElementById("p2").innerHTML="As the letter suggests “V” stands for victory and achieving everything you desire of. These people are born winners and will do anything to reach the top of the world. If you want to defeat them make yourself strong like a mountain. Moreover, these people are great manipulators and can make you feel guilty even if you are not.";
+    }
+    else if(nme=="w"){
+        document.getElementById("p2").innerHTML="People whose name starts with the letter “W” are very charming and you can get easily attracted by their looks. These people are very egoistic about their charms and can easily someone fall in their love. However, you cannot call them flirt, its just their looks that make people fall for them. Apart from this these people are great lovers who can always take stand for the person they love.";
+    }
+    else if (nme=="x") {
+        document.getElementById("p2").innerHTML="People whose name starts with the letter “X” are very dangerous and have the courage to take any risk or any impossible task on their head. These people are always ready to do anything whether its possible or not, they will give it a try. You need to be very careful while talking to them as they might take even a joke seriously which might create problems for them later on.";
+    }
+    else if(nme=="y"){
+        document.getElementById("p2").innerHTML="People falling under the letter “Y” are more into business and real estate. These people just want to make money by hook or by crook. They will try every possible way to earn profit whether it’s investing in gambling or investing in share market. Moreover, these people are very classy and restless in nature and it’s quite tough to make them friends.";
+    }
+    else{
+        document.getElementById("p2").innerHTML="People starting with the letter “Z” are friends to all i.e they are very friendly, polite and love to always surrounded by people. These people love to make friends, share things in order to know about different cultures. Moreover, these people are bit diplomatic and will stand with you at the end but for a certain reason. Basically they have two faces, one is to be friends with everyone and second to satisfy their greed from that friendship.";
+    }
+}
